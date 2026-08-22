f1bbb188e1f0ccc03a6141fa56f076fcb0c98eb6
Rafael_Icaro <rafael_tironi1@estudante.sesisenai.org.br>
2026-08-22 10:09:09 -0300

    add: adicionei o começo do cadastro de tarefa

---
+++ public/cadastro_tarefa.php
@@ -0,0 +1,28 @@
+<?php
+// incluir o arquivo de conexao com o banco de dados
+require_once("database.php");
+
+// verificar se é uma edicao
+
+$edicao = false;
+$tarefa = null;
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $tarefa = obterTarefa($id);
+    if($tarefa){
+        $edicao = true;
+    }
+}
+
+// verificar se o formulário foi enviado
+$mensagem = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id)usuario = $_POST['usuario'] ?? '';
+    $descricao = $_POST['descricao'] ?? '';
+    $setor = $_POST['setor'] ?? '';
+    $prioridade = $_POST['prioridade'] ?? '';
+
+    //validar campos
+    if (empty($usuario) || empty($descricao) || empty($setor) || empty($prioridade)) {
+        
